4a18e6ed440a77ba6fe16ed3abf3f790d41902b2
Raul <rauldondonimachado09@gmail.com>
2024-09-22 18:24:21 -0300

    Making the PlayList row and Props

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -5,15 +5,21 @@ import { BsSpotify } from "react-icons/bs";
 import { Heart } from "lucide-react";
 import PlayButton from '@/assets/PlayButton.svg'
 import Properties from '@/assets/Properties.svg'
+import { FaixasMusic } from "./ui/FaixasMusica";
+
+import Alone from '@/assets/Alone.svg'
+import Beliver from '@/assets/Beliver.svg'
+import Beliver2 from '@/assets/Beliver2.svg'
+import Beliver3 from '@/assets/Beliver3.svg'
 
 export function Music(){
     return (
         <>
-        <div className="font-[Poppins] bg-gradient-to-t from-[#121212] to-[#52534E]  w-full h-full py-4 px-3">
+        <div className="font-[Poppins] bg-gradient-to-t from-[#121212] to-[#52534E] w-full h-full py-4 px-3">
            
             
             <div className="flex  justify-center gap-6 ">
-                <img src={Back} className="w-6 h-7" />
+                <img src={Back} className="w-6 h-7 absolute left-2" />
                 <img src={Folder} className="w-[233.56px] drop-shadow-xl"/>
             </div>
 
@@ -30,6 +36,10 @@ export function Music(){
              </div>  
             <img src={PlayButton} className="size-[60px]"/>
             </div>
+            <FaixasMusic src={Alone} tittle='Alone' autor='Alan walker'/>
+            <FaixasMusic src={Beliver} tittle='Let me love you' autor='Justin Bieber feat DJ Snake'/>
+            <FaixasMusic src={Beliver2} tittle='Ignite' autor='Alan walker'/>
+            <FaixasMusic src={Beliver3} tittle='Taki Taki' autor='DJ Snake feat Selena Gomez'/>
         </div>
         
         </>
